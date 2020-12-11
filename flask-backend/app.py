fcf8b397ac9be6c233caf053c07756cf67e16b17
Spencer Haugh <spencer.haugh@gmail.com>
2020-12-11 18:08:41 -0500

    initial Vue setup complete. Connected /ping to flask backend and receiving correct data. Small victories.

--- flask-backend/app.py
+++ flask-backend/app.py
@@ -1,6 +1,7 @@
 from flask import Flask, jsonify, g
 import pymongo
 from pymongo import MongoClient
+from flask_cors import CORS
 
 # import models
 
@@ -8,38 +9,42 @@ DEBUG = True
 PORT = 8000
 
 app = Flask(__name__)
+app.config.from_object(__name__)
 
-
+# enable CORS
+CORS(app, resources={r'/*': {'origins': '*'}})
 
 
 # MongoDB Connection route to users collection
 client = MongoClient()
 db = client["leftovers-app"]
 users = db.users
-print(users)
+# print(users)
 
 
 
 # DEFAULT ROUTE
 # The default URL ends in /
-@app.route('/')
+@app.route('/', methods=['GET'])
 def index():
 
-
-    # # TEST CONNECTION: INSERT TEST USER
+    # # TEST CONNECTION: INSERT TEST USER ON ROUTE "/" HIT
     # working and connected to DB
     # next step: receive form data from front-end and save to variable user_data as in below example
-    user_data = {
-    'username': 'Bens User',
-    'email': 'newguy@testy.com'
-    }
-    result = users.insert_one(user_data)
-    print('Data from: {0}'.format(result.inserted_id))
+    # user_data = {
+    # 'username': 'Bens User',
+    # 'email': 'newguy@testy.com'
+    # }
+    # result = users.insert_one(user_data)
+    # print('Data from: {0}'.format(result.inserted_id))
 
 
     return 'Welcome to leftovers app!'
 
-
+# sanity check route
+@app.route('/ping', methods=['GET'])
+def ping_pong():
+    return jsonify('pong!')
 
 # Run the app when the program starts!
 if __name__ == '__main__':
