6428b97d87bf4fc7b88dd70cbe5a36aee9b3db29
Spencer Haugh <spencer.haugh@gmail.com>
2020-12-12 12:40:22 -0500

    adding resources folder structure to backend

--- flask-backend/app.py
+++ flask-backend/app.py
@@ -25,18 +25,19 @@ users = db.users
 
 # DEFAULT ROUTE
 # The default URL ends in /
-@app.route('/', methods=['GET'])
+@app.route('/', methods=['POST'])
 def index():
 
-    # # TEST CONNECTION: INSERT TEST USER ON ROUTE "/" HIT
+    # TEST CONNECTION: INSERT TEST USER ON ROUTE "/" POST
     # working and connected to DB
-    # next step: receive form data from front-end and save to variable user_data as in below example
-    # user_data = {
-    # 'username': 'Bens User',
-    # 'email': 'newguy@testy.com'
-    # }
-    # result = users.insert_one(user_data)
-    # print('Data from: {0}'.format(result.inserted_id))
+    # next step: receive form data from front-end and save payload to variable user_data
+    user_data = {
+    'username': 'Bens User',
+    'email': 'newguy@testy.com'
+    'password': '12345'
+    }
+    result = users.insert_one(user_data)
+    print('Data from: {0}'.format(result.inserted_id))
 
 
     return 'Welcome to leftovers app!'
--- flask-backend/models.py
+++ flask-backend/models.py
@@ -3,6 +3,7 @@ from mongoengine import *
 class User(Document):
     username = StringField(required=True)
     email = StringField(required=True)
+    password = StringField(required=True)
     recipes = ListField()
 
 
