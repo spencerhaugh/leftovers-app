fddffecb816fe878a60f9910715401d8dbef8c43
Spencer Haugh <spencer.haugh@gmail.com>
2020-12-13 12:37:24 -0500

    exploring flask api connections. not going well, tbh

--- flask-backend/resources/api.py
+++ flask-backend/resources/api.py
@@ -1,13 +1,19 @@
 # Dependancies
-from flask import request
+from flask import request, Blueprint
 
 # API ROUTES
 
+recipe = 
+
 # GET by ingredient
-@recipe.route('/', methods=['GET'])
+
+# https://api.spoonacular.com/recipes/findByIngredients?ingredients=apple&number=30
+# Example url returns 30 apple based recipes
+
+@recipe.route('/recipes/<ingredient>', methods=['GET'])
 def getRecipes():
-    
+    return 
 
 # retrieves recipes from 3rd party API with one ingredient query param
-# returns (limit 20? 30?) to frontend in an array maybe?
+# returns (limit 30 in the api query example url above) to frontend in an array maybe?
 # front end displays one at a time by calling on the stored array info
